4d85969896b495ffcd1cf014f651632c3115a3cd
abubakr40 <128869848+abubakr40@users.noreply.github.com>
2023-03-27 20:45:43 -0400

    Update Storage-bucket.tf

--- Storage-bucket.tf
+++ Storage-bucket.tf
@@ -1,7 +1,7 @@
 module "gcs_buckets" {
   source  = "terraform-google-modules/cloud-storage/google"
   version = "~> 3.4"
-  project_id  = var.project_id
-  names       = var.bucket_name
+  project_id  = var.summer-heaven-381922
+  names       = var.bucket_OZ2
   prefix = ""
 }
